a1bd8c72bbce1d5956361b4875e883f5a7dcea04
Bartosz Lozinski <lozinski.bart@gmail.com>
2025-05-20 20:39:52 +0200

    First baremetal linking led

---
+++ Inc/main.h
@@ -0,0 +1,14 @@
+/*
+ * main.h
+ *
+ *  Created on: May 20, 2025
+ *      Author: bartoszlozinski
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+//#include <stdint.h>
+#include <stm32l4xx.h>
+
+#endif /* MAIN_H_ */
