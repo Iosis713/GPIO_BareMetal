8d38481a3173d1d383285ef455741604c681961d
Bartosz Lozinski <lozinski.bart@gmail.com>
2025-06-09 20:21:47 +0200

    Ospeedr register configured into compiletime

--- Inc/Gpio.hpp
+++ Inc/Gpio.hpp
@@ -14,6 +14,10 @@
 
 enum class OptionsPUPDR
 {
+	// GPIOx_PUPDR (GPIO port pull-up/pull-down register):
+    // Reset value: 0x6400 0000 (for port A) - pin15 [pull-up], pin14 [pull-down], others [No pull-up/pull-down]
+	// Reset value: 0x0000 0100 (for port B) - pin4 [pull-up]
+	// Reset value: 0x0000 0000 (for other ports)
     None, // 00 = No pull-up, no pull-down
     PullUp, // 01 = Pull-up
     PullDown,// 10 = Pull-down
@@ -26,6 +30,31 @@ enum class OptionsOTYPER
 	//reset value 0x0000 0000
 	PushPull, // 0 = Output push-pull (reset state)
 	OpenDrain,// 1 = Output open-drain
+};
+
+enum class OptionsMODER
+{
+	// STM32L476RGT6 Reference Manual:
+	// GPIOx_MODER (GPIO port mode register) controls the mode of each pin
+	//Reset value: 0xABFF FFFF (port A) - 1010 1011 1111 1111 1111 1111 1111 1111
+	//Reset value: 0xFFFF FEBF (port B) - 1111 1111 1111 1111 1111 1110 1011 1111
+	Input, // 00 = Input mode
+	Output, // 01 = General purpose output mode
+	Alternate, // 10 = Alternate function mode
+	Analog, // 11 = Analog mode (reset state for most cases)
+};
+
+enum class OptionsOSPEEDR
+{
+	// STM32L476RGT6 RM: 8.5.3 GPIO port output speed register
+	// Reset value: 0x0C00 0000 for port A
+	// Reset value: 0x0000 0000 for other ports
+	LowSpeed, // 00
+	MediumSpeed, // 01
+	HighSpeed, // 10
+	VeryHighSpeed, //11
+	//Refer to the device datasheet for the frequency specifications
+	//and the power supply and load conditions for each speed...
 };
 
 template<typename Derived>
@@ -50,10 +79,6 @@ public:
 	template<OptionsPUPDR pupdrOption>
 	void ConfigurePUPDR()
 	{
-	    // GPIOx_PUPDR (GPIO port pull-up/pull-down register):
-		// Reset value: 0x6400 0000 (for port A) - pin15 [pull-up], pin14 [pull-down], others [No pull-up/pull-down]
-		// Reset value: 0x0000 0100 (for port B) - pin4 [pull-up]
-		// Reset value: 0x0000 0000 (for other ports)
 		using enum OptionsPUPDR;
 		auto port = this->Port();
 		port->PUPDR &= ~(PUPDR_MASKS[Derived::pin]); //00- no pull-up, no pull-down
@@ -74,39 +99,54 @@ public:
 			port->OTYPER |= OTYPER_BITS[Derived::pin];
 	}
 
+	template<OptionsMODER moderOption>
+	void ConfigureMODER()
+	{
+		using enum OptionsMODER;
+		auto port = this->Port();
+		port->MODER &= ~(MODER_MASKS[Derived::pin]); //reset to 00
+		if constexpr (moderOption == Input)
+			return;
+		else if constexpr (moderOption == Output)
+			port->MODER |= MODER_MASKS_0[Derived::pin];
+		else if constexpr (moderOption == Alternate)
+			port->MODER |= MODER_MASKS_1[Derived::pin];
+		else if constexpr (moderOption == Analog)
+			port->MODER |= MODER_MASKS[Derived::pin];
+	}
+
+	template<OptionsOSPEEDR ospeedrOption>
+	void ConfigureOSPEEDR()
+	{
+		using enum OptionsOSPEEDR;
+		auto port = this->Port();
+		port->OSPEEDR &= ~OSPEEDR_MASKS[Derived::pin];
+		if constexpr (ospeedrOption == LowSpeed)
+			return;
+		else if constexpr (ospeedrOption == MediumSpeed)
+			port->OSPEEDR |= OSPEEDR_MASKS_0[Derived::pin];
+		else if constexpr (ospeedrOption == HighSpeed)
+			port->OSPEEDR |= OSPEEDR_MASKS_1[Derived::pin];
+		else if constexpr (ospeedrOption == VeryHighSpeed)
+			port->OSPEEDR |= OSPEEDR_MASKS[Derived::pin];
+	}
+
 };
 
 template<std::uintptr_t portAddr_
         , uint8_t pin_
 		, OptionsOTYPER otyperOption = OptionsOTYPER::PushPull
+		, OptionsOSPEEDR ospeedrOption = OptionsOSPEEDR::LowSpeed
 		, OptionsPUPDR pupdrOption = OptionsPUPDR::None>
-class GpioOutput : public IGpio<GpioOutput<portAddr_, pin_, otyperOption, pupdrOption>>
+class GpioOutput : public IGpio<GpioOutput<portAddr_, pin_, otyperOption, ospeedrOption, pupdrOption>>
 {
 private:
 	void ConfigureAsOutput()
 	{
-		auto port = this->Port();
 		static_assert(pin >= 0 && pin < 16, "Invalid pin number: needs to be in range of 0 - 15!");
-
-		// STM32L476RGT6 Reference Manual:
-		// GPIOx_MODER (GPIO port mode register) controls the mode of each pin.
-		// 00 = Input mode
-		// 01 = General purpose output mode
-		// 10 = Alternate function mode
-		// 11 = Analog mode (reset state)
-		port->MODER &= ~MODER_MASKS[pin];
-		port->MODER |= MODER_OUTPUT_BITS[pin];
-
+		this->template ConfigureMODER<OptionsMODER::Output>();
 		this->template ConfigureOTYPER<otyperOption>();
-		port->OTYPER &= ~OTYPER_BITS[pin];
-
-		// GPIOx_OSPEEDR (GPIO port output speed register):
-		// 00 = Low speed (reset value)
-		// 01 = Medium speed
-		// 10 = High speed
-		// 11 = Very high speed
-		port->OSPEEDR &= ~OSPEEDR_MASKS[pin];
-
+		this->template ConfigureOSPEEDR<ospeedrOption>();
 		this->template ConfigurePUPDR<pupdrOption>();
 
 	}
@@ -135,16 +175,17 @@ public:
 template<std::uintptr_t portAddr_
         , uint8_t pin_
 		, OptionsOTYPER otyperOption = OptionsOTYPER::PushPull
+		, OptionsOSPEEDR ospeedrOption = OptionsOSPEEDR::LowSpeed
 		, OptionsPUPDR pupdrOption = OptionsPUPDR::None>
-class GpioInput : public IGpio<GpioInput<portAddr_, pin_, otyperOption, pupdrOption>>
+class GpioInput : public IGpio<GpioInput<portAddr_, pin_, otyperOption, ospeedrOption, pupdrOption>>
 {
 protected:
 	void ConfigureAsInput()
 	{
-		auto port = this->Port();
 		static_assert(pin >= 0 && pin < 16, "Invalid pin number: needs to be in range of 0 - 15!");
-		port->MODER &= ~(MODER_MASKS[pin]); //00 - input
+		this->template ConfigureMODER<OptionsMODER::Input>();
 		this->template ConfigureOTYPER<otyperOption>();
+		this->template ConfigureOSPEEDR<ospeedrOption>();
 		this->template ConfigurePUPDR<pupdrOption>();
 	}
 public:
