37430b8b5feab979135e6fa2a3f316529e78bc8b
Bartosz Lozinski <lozinski.bart@gmail.com>
2025-06-11 18:13:25 +0200

    Configure Capture compare and interrupts separately in PWM and PWMchannel

--- Inc/Pwm.hpp
+++ Inc/Pwm.hpp
@@ -41,35 +41,48 @@ class PWM
 {
 private:
 	static constexpr std::uintptr_t timerAddr = timerAddr_;
-	void EnableTimerClock()
-	{
-		if constexpr (timerAddr == TIM2_BASE)
-			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
-		else if constexpr (timerAddr == TIM3_BASE)
-			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM3EN;
-		else if constexpr (timerAddr == TIM4_BASE)
-			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM4EN;
-		else if constexpr (timerAddr == TIM5_BASE)
-			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM5EN;
-		else if constexpr (timerAddr == TIM6_BASE)
-			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM6EN;
-		else if constexpr (timerAddr == TIM7_BASE)
-			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM7EN;
-	}
-
-	void InterruptsConfiguration()//TIM3
+	void EnableTimerClockAndInterruptsConfig(const uint8_t priority)
 	{
 		//RM 31.4.4 DMA/Interrupt enable register (DIER)
-		//Trigger interrupt enable TIE (on/off - 1/0)
-		//Update interrupt enable UIE (on/off - 1/0)
-
-		auto tim = Tim();
+		auto tim = Tim(); //Update interrupt enable UIE (on/off - 1/0)
 		tim->DIER |= TIM_DIER_UIE; //update interrupt is enabled
-		tim->DIER |= TIM_DIER_CC1IE; //capture/compare 1 interrupt 1 (CH1) enable
 
-		//priority and enable in nvic; enum from stm32l476xx.h (CMSIS)
-		NVIC_SetPriority(TIM3_IRQn, 1);
-		NVIC_EnableIRQ(TIM3_IRQn);
+		if constexpr (timerAddr == TIM2_BASE)
+		{
+			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN; //timer clock
+			NVIC_SetPriority(TIM2_IRQn, priority);
+			NVIC_EnableIRQ(TIM2_IRQn);
+		}
+		else if constexpr (timerAddr == TIM3_BASE)
+		{
+			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM3EN;
+			NVIC_SetPriority(TIM3_IRQn, priority);
+			NVIC_EnableIRQ(TIM3_IRQn);
+		}
+		else if constexpr (timerAddr == TIM4_BASE)
+		{
+			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM4EN;
+			NVIC_SetPriority(TIM4_IRQn, priority);
+			NVIC_EnableIRQ(TIM4_IRQn);
+		}
+		else if constexpr (timerAddr == TIM5_BASE)
+		{
+			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM5EN;
+			NVIC_SetPriority(TIM5_IRQn, priority);
+			NVIC_EnableIRQ(TIM5_IRQn);
+		}
+		else if constexpr (timerAddr == TIM6_BASE)
+		{
+			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM6EN;
+			NVIC_SetPriority(TIM6_IRQn, priority);
+			NVIC_EnableIRQ(TIM6_IRQn);
+		}
+		else if constexpr (timerAddr == TIM7_BASE)
+		{
+			RCC->APB1ENR1 |= RCC_APB1ENR1_TIM7EN;
+			NVIC_SetPriority(TIM7_IRQn, priority);
+			NVIC_EnableIRQ(TIM7_IRQn);
+		}
 	}
 
 public:
@@ -78,28 +91,25 @@ public:
 	PWM(PWM&& source) = delete;
 	PWM& operator=(const PWM& source) = delete;
 	PWM& operator=(PWM&& source) = delete;
-	PWM()
+	PWM() = delete;
+	PWM(const uint8_t priority)
 	{
 		//PWM
 		//Timer clock - RM 6.2. Tim3 PCLK x1 or x2 -> TIMPCLK
 		//HPRE = 0x00 --> div 1
 		//PPRE1 = 0x00 --> div 1 (APB1PRE)
 		//RM 6.4.19 APB1ENR -- 1 enabled, 0 - disabled
-		EnableTimerClock();
+		EnableTimerClockAndInterruptsConfig(priority);
 		auto tim = Tim();
 		//TIMx_prescaler 16bit; RM 31.4.14 TIMx prescaler
 		tim->PSC = prescalerPSC_; //should be 1 less, than divider
 		tim->ARR = ARR_; //should be 1 less than divider
 		//TIMER counter register
 		tim->CNT = 0;//reset if it had different value
-		//TIMER counter register
-		tim->CNT = 0;//reset if it had different value
 		//RM 31.4.1 CEN bit (Counter enable)
 		tim->CR1 |= TIM_CR1_CEN;
-		InterruptsConfiguration();
 	}
 
-	//TIM3
 	void InterruptHandler()
 	{
 		//RM 31.4.6 TIMx Status register (TIMx_SR) -- UIF (update interrupt flag) rc_w0 readClear_with0
@@ -139,20 +149,59 @@ private:
 		this->template ConfigureOTYPER<otyperOption>();
 		this->template ConfigureOSPEEDR<ospeedrOption>();
 		this->template ConfigurePUPDR<pupdrOption>();
-		//RM 31.4.8 TIMx capture/compare mode register 1 (TIMx_CCMR1) [alternate] - output
+		ConfigureCaptureCompare();
+
+	}
+
+	void ConfigureCaptureCompare()
+	{
+		//RM 31.4.8 TIMx capture/compare mode register 1/2 (TIMx_CCMR1) [alternate] - output
 		//Capture/Compare 1 Selection -CC1S - is output (0) by default, but check this bit
 		//Output Compare 1 Mode - OC1M - PWM mode 1 -active when CNT < CCR1
-		//0110  for PWM mode 1
-		timer->CCMR1 |= TIM_CCMR1_OC1M_1;
-		timer->CCMR1 |= TIM_CCMR1_OC1M_2;
+		if (channel == 1)
+		{
+			timer->CCMR1 |= TIM_CCMR1_OC1M_1; //0110  for PWM mode 1
+			timer->CCMR1 |= TIM_CCMR1_OC1M_2;
+			// Enable preload for CCR1 (this is **required** for PWM to work)
+			timer->CCMR1 |= TIM_CCMR1_OC1PE;
+			timer->CCR1 = 0;// just for initialization clearing the counter
+			//capture/compare output enable bit
+			timer->CCER |= TIM_CCER_CC1E; //Capture/compare enable register (TIMx_CCER1) RM 31.4.11
 
-		// Enable preload for CCR1 (this is **required** for PWM to work)
-		timer->CCMR1 |= TIM_CCMR1_OC1PE;
+			//RM 31.4.4 DMA/Interrupt enable register (DIER)
+			//Trigger interrupt enable TIE (on/off - 1/0)
+			timer->DIER |= TIM_DIER_CC1IE; //capture/compare 1 interrupt 1 (CH1) enable
+		}
+		else if (channel == 2)
+		{
+			timer->CCMR1 |= TIM_CCMR1_OC2M_1; //refer to OM1M
+			timer->CCMR1 |= TIM_CCMR1_OC2M_2;
+			timer->CCMR1 |= TIM_CCMR1_OC2PE;
+			timer->CCR2 = 0;
+			timer->CCER |= TIM_CCER_CC2E;
+			timer->DIER |= TIM_DIER_CC2IE;
+		}
+		else if (channel == 3)
+		{
+			timer->CCMR2 |= TIM_CCMR2_OC3M_1; //refer to OM1M
+			timer->CCMR2 |= TIM_CCMR2_OC3M_2;
+			timer->CCMR2 |= TIM_CCMR2_OC3PE;
+			timer->CCR3 = 0;
+			timer->CCER |= TIM_CCER_CC3E;
+			timer->DIER |= TIM_DIER_CC3IE;
 
-		timer->CCR1 = 0;// just for initialization clearing the counter
-		//capture/compare output enable bit
-		timer->CCER |= TIM_CCER_CC1E; //Capture/compare enable register (TIMx_CCER1) RM 31.4.11
+		}
+		else if (channel == 4)
+		{
+			timer->CCMR2 |= TIM_CCMR2_OC4M_1; //refer to OM1M
+			timer->CCMR2 |= TIM_CCMR2_OC4M_2;
+			timer->CCMR2 |= TIM_CCMR2_OC4PE;
+			timer->CCR4 = 0;
+			timer->CCER |= TIM_CCER_CC4E;
+			timer->DIER |= TIM_DIER_CC4IE;
+		}
 	}
+
 
 public:
 	static constexpr std::uintptr_t portAddr = portAddr_;
