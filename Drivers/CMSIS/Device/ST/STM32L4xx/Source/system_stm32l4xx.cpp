a783be48b352e4a66344b0669dc87de729597d49
Bartosz Lozinski <lozinski.bart@gmail.com>
2025-07-26 15:38:12 +0200

    Revert "Removed CMSIS from control version"
    This reverts commit 9b886c075ab074107af34475a84a3d3c0ebf8127.

---
+++ Drivers/CMSIS/Device/ST/STM32L4xx/Source/system_stm32l4xx.cpp
@@ -0,0 +1,27 @@
+/*
+ * system_stm32l4xx.cpp
+ *
+ *  Created on: Jun 17, 2025
+ *      Author: bartoszlozinski
+ */
+
+#include "stm32l4xx.h"
+
+extern "C" {
+
+// Default system clock after reset is MSI at 4 MHz
+uint32_t SystemCoreClock = 4000000U;
+
+void SystemInit() {
+    // You can leave this empty if you're not modifying clocks or peripherals here.
+    // Optionally, disable watchdogs, etc.
+	SCB->CPACR |= (0xF << 20); //enable FPU 20, 22
+	SystemCoreClockUpdate();
+}
+
+void SystemCoreClockUpdate() {
+    // No dynamic clock switching in this setup — just hardcode MSI at 4 MHz
+    SystemCoreClock = 4000000U;
+}
+
+}
