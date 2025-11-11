d8464e17518db032ff7301f41362a5b228fbc3f4
Bartosz Lozinski <lozinski.bart@gmail.com>
2025-11-11 16:30:55 +0100

    Added WaitUntil inline functions instead of raw registers operations

--- Inc/I2C.hpp
+++ Inc/I2C.hpp
@@ -62,40 +62,13 @@ protected:
 		static_cast<Derived*>(this)->i2c->CR1 |= I2C_CR1_PE;
 	}
 
-	inline void SetTimingRegister(const uint32_t timingRegister)
-	{
-		static_cast<Derived*>(this)->i2c->TIMINGR = timingRegister;
-	}
-
-	inline bool IsrBusy()
-	{
-		return (static_cast<Derived*>(this)->i2c->ISR & I2C_ISR_BUSY); // true = busy, false = idle
-	}
-
-	inline bool IsrTxisEmpty()
-	{
-		return (static_cast<Derived*>(this)->i2c->ISR & I2C_ISR_TXIS); // true = empty (can write data), false = not empty
-	}
-
-	inline bool IsrTransferCompleted()
-	{
-		return (static_cast<Derived*>(this)->i2c->ISR & I2C_ISR_TC); //true =TransferCompleted
-	}
-
-	inline bool IsrStopDetected()
-	{
-		return (static_cast<Derived*>(this)->i2c->ISR & I2C_ISR_STOPF); // true = stop detected
-	}
-
-	inline bool IsrReceiveDataNotEmpty()
-	{
-		return (static_cast<Derived*>(this)->i2c->ISR & I2C_ISR_RXNE); //true = data avaiable in RXDR
-	}
-
-	inline void StopDetectionClear()
-	{
-		static_cast<Derived*>(this)->i2c->ICR |= I2C_ICR_STOPCF;
-	}
+	inline void SetTimingRegister(const uint32_t timingRegister){ static_cast<Derived*>(this)->i2c->TIMINGR = timingRegister; }
+	inline bool IsrBusy() { return (static_cast<Derived*>(this)->i2c->ISR & I2C_ISR_BUSY); } // true = busy, false = idle
+	inline bool IsrTxisEmpty() { return (static_cast<Derived*>(this)->i2c->ISR & I2C_ISR_TXIS); } // true = empty (can write data), false = not empty
+	inline bool IsrTransferCompleted() { return (static_cast<Derived*>(this)->i2c->ISR & I2C_ISR_TC); } //true =TransferCompleted
+	inline bool IsrStopDetected() {	return (static_cast<Derived*>(this)->i2c->ISR & I2C_ISR_STOPF); } // true = stop detected
+	inline bool IsrReceiveDataNotEmpty() { return (static_cast<Derived*>(this)->i2c->ISR & I2C_ISR_RXNE); } //true = data avaiable in RXDR
+	inline void StopDetectionClear() { static_cast<Derived*>(this)->i2c->ICR |= I2C_ICR_STOPCF;	}
 };
 
 template<typename I2CStruct>
@@ -104,6 +77,11 @@ class I2c : public I2cInterface<I2c<I2CStruct>>
 	friend class I2cInterface<I2c<I2CStruct>>;
 protected:
 	volatile I2CStruct* const i2c = nullptr;
+	inline void WaitWhileIsrBusy() {while (this->IsrBusy()) {}} //wait if bus is busy
+	inline void WaitUntilTXBufferIsReady() {while (!this->IsrTxisEmpty()) {};}	//wait until TX buffer is ready, then send memory address
+	inline void WaitUntilTransferCompleted() { while (!this->IsrTransferCompleted()) {};} //wait Until transfer complete (NBYTES sent)
+	inline void WaitUntilIsrStopDetected() {while (!this->IsrStopDetected()) {};}	 //Wait for STOP to complete (optional, just to ensure bus is idle
+	inline void WaitUntilRxBufferNotEmpty() {while (!this->IsrReceiveDataNotEmpty()) {}} //Wait until RX buffer not empty - RM 39.9.8 Receive data register not empty. Cleared when RXDX is read
 
 public:
 	I2c(const I2c& source) = delete;
@@ -123,30 +101,30 @@ public:
 		if (size == 0 || size > 8) //24AA01 has 8-byte write page limit
 			return false;
 
-		while (this->IsrBusy()) {} //wait if bus is busy
+		WaitWhileIsrBusy();
 
 		//Configure I2C1 to write:
 		// - dev_addr: 8-bit address (e.g. 0xA0 = 0x50 << 1)
 		// - (1 + len): 1 byte for mem_addr + N data bytes
 		// - START: trigger start condition
 
-		i2c->CR2 = (devAddr & 0xFE)						//Ensure LSB=0 for write (xxxx xxx0)
+		i2c->CR2 = (devAddr & 0xFE)							//Ensure LSB=0 for write (xxxx xxx0)
 			      | ((size + 1) << I2C_CR2_NBYTES_Pos)		//total number of bytes to send
 				  | I2C_CR2_START;							//generate start condition
 
-		while (!this->IsrTxisEmpty()) {};	//wait until TX buffer is ready, then send memory address
+		WaitUntilTXBufferIsReady();
 		i2c->TXDR = memAddr;
 
 		//send each byte of data
 		for (std::size_t i = 0; i < size; ++i)
 		{
-			while (!this->IsrTxisEmpty()) {} // Wait for TX buffer ready
+			WaitUntilTXBufferIsReady();
 			i2c->TXDR = data[i];
 		}
 
-		while (!this->IsrTransferCompleted()) {} //wait Until transfer complete (NBYTES sent)
+		WaitUntilTransferCompleted();
 		i2c->CR2 |= I2C_CR2_STOP;			 //Send STOP condition
-		while (!this->IsrStopDetected()) {}	 //Wait for STOP to complete (optional, just to ensure bus is idle
+		WaitUntilIsrStopDetected();
 		this->StopDetectionClear();
 		return true;
 	}
@@ -156,15 +134,15 @@ public:
 		if (size == 0)
 			return false;
 
-		while (this->IsrBusy()) {} // Wait until I2C bus is idle
+		WaitWhileIsrBusy();
 		// First, send a dummy write to set EEPROM's itnernal memory address pointer
 		i2c->CR2 = (devAddr & 0xFE)			// Write address (LSB = 0)
 				  | (1 << I2C_CR2_NBYTES_Pos)	//Send 1 bte (memory address)
 				  | I2C_CR2_START;				// Generate start condition
 
-		while (!this->IsrTxisEmpty()) {}  //Wait for TX ready
+		WaitUntilTXBufferIsReady();
 		i2c->TXDR = memAddr;					//send memory address
-		while (!this->IsrTransferCompleted()) {}	//wait until transfer complete
+		WaitUntilTransferCompleted();
 
 		//Read: repeat start - read
 		i2c->CR2 = (devAddr & 0xFE)				// Device address
@@ -175,13 +153,13 @@ public:
 		//Read loop
 		for (size_t i = 0; i < size; ++i)
 		{
-			while (!this->IsrReceiveDataNotEmpty()) {} //Wait until RX buffer not empty - RM 39.9.8 Receive data register not empty. Cleared when RXDX is read
+			WaitUntilRxBufferNotEmpty();
 			data[i] = static_cast<uint8_t>(I2C1->RXDR);	//register is 32bit, even if data is only 8 bit
 		}
 
-		while (!this->IsrTransferCompleted()) {}		//Wait until all bytes received; CR39.9.7. - Transfer complete (master mode)
+		WaitUntilTransferCompleted();//Wait until all bytes received; CR39.9.7. - Transfer complete (master mode)
 		i2c->CR2 |= I2C_CR2_STOP;					//Send STOP condition
-		while (!this->IsrStopDetected()) {}			//Wait for STOP to complete
+		WaitUntilIsrStopDetected();
 		this->StopDetectionClear();
 		return true;
 	}
